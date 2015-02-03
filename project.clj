01cf8e39ee16c25bd419053f670a87e148b475bf
Anna Pawlicka <anna@mastodonc.com>
2015-02-03 16:39:29 +0000

    Bump version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/ckan "0.1.1-SNAPSHOT"
+(defproject kixi/ckan "0.1.2"
   :description "Clojure client for the CKAN API."
   :url "https://github.com/MastodonC/kixi.ckan"
   :license {:name "Eclipse Public License"
